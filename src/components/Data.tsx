81bb92639a2de8b6559989cb11c3b7640394f10f
Ade Adeola <adedotster@gmail.com>
2021-02-18 20:56:59 +0000

    data table created

---
+++ src/components/Data.tsx
@@ -0,0 +1,34 @@
+import Table from "react-bootstrap/Table";
+
+type datum = {
+  title: string;
+  rating: number;
+  duration: string;
+};
+
+interface TableProps {
+  data: Array<datum>;
+}
+export default function Data({ data }: TableProps) {
+  return (
+    <Table>
+      <tr>
+        <th>Movie</th>
+        <th>Duration</th>
+        <th>Rating</th>
+      </tr>
+      {data.map(
+        (
+          item: { title: string; duration: string; rating: number },
+          idx: number
+        ) => (
+          <tr key={idx}>
+            <td>{item.title}</td>
+            <td>{item.duration}</td>
+            <td>{item.rating}</td>
+          </tr>
+        )
+      )}
+    </Table>
+  );
+}
