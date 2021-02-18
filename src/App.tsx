718f07140086d707436c3f3fa294aa8442f1b5e2
Ade Adeola <adedotster@gmail.com>
2021-02-18 21:29:34 +0000

    project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import RatingInput from "./components/RatingInput";
+import TextInput from "./components/TextInput";
+import Submit from "./components/Submit";
+import api from "./data.json";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Data from "./components/Data";
+import { Form } from "react-bootstrap";
 
 function App() {
+  const [movie, setMovie] = useState("");
+  const [rating, setRating] = useState(0);
+  const [duration, setDuration] = useState("");
+  const [data, setData] = useState([] as any);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setData(api);
+  }, []);
+
+  const setAverage = (
+    initialRating: number,
+    size: number,
+    newRating: number
+  ) => {
+    return (initialRating * size + newRating) / (size + 1);
+  };
+
+  const handleSubmit = () => {
+    const movieList = data.map((item: { title: string }) => item.title);
+
+    if (movieList.includes(movie)) {
+      const index = movieList.indexOf(movie);
+      const movieData = data[index];
+      const newRating = setAverage(movieData.rating, movieData.votes, rating);
+      setData([
+        ...data.slice(0, index),
+        {
+          title: movie,
+          rating: newRating,
+          duration,
+          votes: movieData.votes + 1,
+        },
+        ...data.slice(index + 1),
+      ]);
+    } else setData([...data, { title: movie, rating, duration, votes: 1 }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Rate your favorite movies</h1>
+      <Form>
+        <TextInput
+          value={movie}
+          onChange={(e) => setMovie(e.target.value)}
+          placeholder="Title of movie"
+          label="Movie"
+          list="browsers"
+          required
+        />
+        <datalist id="browsers">
+          {data.map((item: { title: string }) => (
+            <option value={item.title} key={item.title} />
+          ))}
+        </datalist>
+        <RatingInput
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+          placeholder="Scale of 0 to 5"
+          label="Rating"
+          required
+        />
+        <TextInput
+          value={duration}
+          onChange={(e) => setDuration(e.target.value)}
+          placeholder="How long the movie took"
+          label="Duration"
+          required
+        />
+        <Submit onClick={handleSubmit} />
+      </Form>
+      <br />
+      <TextInput
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Filter movies"
+        label="Search"
+      />
+      <Data data={data} filter={search} />
+    </Container>
   );
 }
 
--- src/components/Submit.tsx
+++ src/components/Submit.tsx
@@ -2,11 +2,12 @@ import Button from "react-bootstrap/Button";
 
 interface SubmitProps {
   onClick: (e: any) => void;
+  [props: string]: any;
 }
 
-export default function Submit({ onClick }: SubmitProps) {
+export default function Submit({ onClick, props }: SubmitProps) {
   return (
-    <Button variant="primary" onClick={onClick}>
+    <Button variant="primary" onClick={onClick} {...props}>
       Submit
     </Button>
   );
